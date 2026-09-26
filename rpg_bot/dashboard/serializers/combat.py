de31fe521d0757f1832449806eb19436c363438d
Elliott <sendtoelle@hotmail.com>
2026-09-26 14:37:39 +0200

    Expose Dash state in combat dashboard

--- rpg_bot/dashboard/serializers/combat.py
+++ rpg_bot/dashboard/serializers/combat.py
@@ -220,6 +220,7 @@ def _combat_scene_data(scene: CombatScene, world: WorldService) -> JsonObject:
                 "is_between_landmarks": combatant.is_between_landmarks,
                 "standard_action_spent": combatant.standard_action_spent,
                 "defending": combatant.defending,
+                "dashed": combatant.dashed,
                 "usable_items": _combatant_usable_items_data(
                     world,
                     combatant.kind.value,
